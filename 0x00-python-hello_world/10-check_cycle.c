7553886766b5ecbda2eb81b1e6e97fae46bbaad6
maxgate <kelechivincy@gmail.com>
2022-09-21 08:29:17 -0700

    submitted

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - Check if a singly-linked list contains a cycle.
+ * @list: A singly-linked list.
+ *
+ * Return: If there is no cycle - 0.
+ * 	   If there is a cycle - 1.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *bicycle, *car;
+	if (list == NULL || list ->next == NULL)
+		return (0);
+
+	bicycle = list->next;
+	car = list->next->next;
+
+	while (bicycle && car && cat->next)
+	{
+		if (bicycle == car)
+			return (1);
+
+		bicycle = bicycle->next;
+		car = car->next->next;
+	}
+
+	return (0);
+}
